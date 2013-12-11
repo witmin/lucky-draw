7604c170c848d27d3a6848b9ce2a4d32eee7e25e
Millie Lin <linxinmin@gmail.com>
2013-12-11 16:46:54 +0800

    Added Start Button

--- html-templates/js/view.js
+++ html-templates/js/view.js
@@ -24,4 +24,34 @@ $(document).ready(function(){
     $('.item-list li').mouseleave(function(){
         $(this).removeClass("hover");
     });
+
+//    Define the responsive round START button
+    var winHeight = $(window).height();
+
+    var updateStartButtonStyle = function(){
+        winHeight = $(window).height();
+        $('.start-btn').css({
+            'height' :  winHeight/2,
+            'width' : winHeight/2,
+            'border-radius': ($(this).width())/2
+        });
+        $('.start-btn i.fa-compass').css({
+            'font-size': winHeight/($(this).width())*10  + 'em'
+        });
+        $('.start-btn span.text').css({
+            'font-size': winHeight/($(this).width())*10  + 'em'
+        });
+    }
+
+    updateStartButtonStyle();
+    $(window).resize(function(){
+        updateStartButtonStyle();
+    });
+
+    var textGo = $('');
+    $('.start-btn').mouseenter(function(){
+        $(this).children('i.fa-compass').hide();
+
+    });
+
 });
