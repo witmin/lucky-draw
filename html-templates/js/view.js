59563cd04b074dad4c371a765b15695501422e29
mike.chung <mikechung4@gmail.com>
2013-12-11 23:08:19 +0800

    random result will add a selected class there

--- html-templates/js/view.js
+++ html-templates/js/view.js
@@ -1,57 +1,160 @@
 /**
  * Created by millie.lin on 12/9/13.
  */
-$(document).ready(function(){
-//    Page Loading Animate
-    $('.logo').addClass('animated slideInLeft');
-//    Tooltip
-    $('.tooltip').hide();
-    $('.tooltip-container').mouseenter(function(){
-        $('.tooltip').slideDown('fast');
+(function($, window, document) {
 
-    });
-    $('.tooltip-container').mouseleave(function(){
-        $('.tooltip').slideUp('fast');
-    });
+    var Machine = function(updateCandidatesCallback, resultCallback) {
+
+        var removeCandidate = function (v) {
+            $.get('/removeCandidate', { 'candidate': v }, function() {});
+        };
+
+        updateCandidatesCallback = updateCandidatesCallback || function(candidates) {
+
+            var $candidateList = $('#candidate ul');
+            $candidateList.empty();
+            $.each(candidates, function(i, v) {
+                $candidateList.append($("<li></li>").append(v).append($('<button></button>').text('remove').bind('click', function() {
+                    removeCandidate(v);
+                })));
+            });
+        };
+
+        resultCallback = resultCallback || function(poorMan) {
+            $('#poor-man').text(poorMan);
+        };
+
+        var sockjs_url = '/sock';
+        var sockjs = new SockJS(sockjs_url);
+
+        sockjs.onopen    = function()  {
+
+        };
+
+        sockjs.onmessage = function(e) {
+            var obj = JSON.parse(e.data);
+            // Super lazy implementation
+            if (obj.candidates) {
+                updateCandidatesCallback(obj.candidates);
+            } else if (obj.poorMan) {
+                resultCallback(obj.poorMan);
+            }
+
+        };
+
+        sockjs.onclose   = function()  {
+
+        };
+
+        var $candidate = $('#candidate form');
+        $candidate.submit(function() {
+
+            return false;
+        });
+
+        $('#rand').bind('click', function() {
+            $.get('/rand', {}, function(){});
+        });
+        return {
+            addCandidate: function(v) {
+                $.get('/addCandidate', {'candidate': v}, function() {});
+            },
+            removeCandidate: removeCandidate,
+            rand: function() {
+                $.get('/rand', {}, function(){});
+            }
+        }
+    };
+
+    $(document).ready(function(){
+        var machine = new Machine(function(candidates) {
+            var $candidateList = $('ul.item-list');
+            $candidateList.empty();
+            $.each(candidates, function(i, v) {
+                $candidateList.append($("<li></li>").prop('id', v).mouseenter(function(){
+                    $(this).addClass("hover");
+                }).mouseleave(function(){
+                        $(this).removeClass("hover");
+                    })
+                    .append(v)
+                    .append($('<span href="" class="delete"><i class="fa fa-minus-circle"></i></span>').bind('click', function() {
+                        // Lazy implementation :P
+                        machine.removeCandidate($(this).parent().prop('id'));
+                    })));
+            });
+        }, function(poorMan) {
+            $('.item-list li').removeClass('selected');
+            $('li#' + poorMan).addClass('selected');
+        });
+
+        $('button[title="Add"]').bind('click', function() {
+            var textField = $('#new-candidate');
+            machine.addCandidate(textField.val());
+            textField.reset();
+        });
+
+        $('#edit-item-form').submit(function() {
+            return false;
+        });
+
+//    Page Loading Animate
+        $('.logo').addClass('animated slideInLeft');
+//    Tooltip
+        $('.tooltip').hide();
+        $('.tooltip-container').mouseenter(function(){
+            $('.tooltip').slideDown('fast');
+
+        });
+        $('.tooltip-container').mouseleave(function(){
+            $('.tooltip').slideUp('fast');
+        });
 
 // hover Added Items to add hover class
-    var deleteBtn = $('<a href="" class="delete"><i class="fa fa-minus-circle"></i></a>')
-    $('.item-list li').append(deleteBtn);
-    $('.item-list li').children('.delete');
-    $('.item-list li').mouseenter(function(){
-        $(this).addClass("hover");
-    });
-    $('.item-list li').mouseleave(function(){
-        $(this).removeClass("hover");
-    });
+//        var deleteBtn = $('<span href="" class="delete"><i class="fa fa-minus-circle"></i></span>').bind('click', function() {
+//            // Lazy implementation :P
+//            machine.removeCandidate("");
+//        });
+//        $('.item-list li').append(deleteBtn);
+//        $('.item-list li').children('.delete');
+//        $('.item-list li').mouseenter(function(){
+//            $(this).addClass("hover");
+//        });
+//        $('.item-list li').mouseleave(function(){
+//            $(this).removeClass("hover");
+//        });
 
 //    Define the responsive round START button
-    var winHeight = $(window).height();
+        var winHeight = $(window).height();
 
-    var updateStartButtonStyle = function(){
-        winHeight = $(window).height();
-        $('.start-btn').css({
-            'height' :  winHeight/2,
-            'width' : winHeight/2,
-            'border-radius': ($(this).width())/2
-        });
-        $('.start-btn i.fa-compass').css({
-            'font-size': winHeight/($(this).width())*10  + 'em'
-        });
-        $('.start-btn span.text').css({
-            'font-size': winHeight/($(this).width())*10  + 'em'
-        });
-    }
+        var updateStartButtonStyle = function(){
+            winHeight = $(window).height();
+            $('.start-btn').css({
+                'height' :  winHeight/2,
+                'width' : winHeight/2,
+                'border-radius': ($(this).width())/2
+            });
+            $('.start-btn i.fa-compass').css({
+                'font-size': winHeight/($(this).width())*10  + 'em'
+            });
+            $('.start-btn span.text').css({
+                'font-size': winHeight/($(this).width())*10  + 'em'
+            });
+        }
 
-    updateStartButtonStyle();
-    $(window).resize(function(){
         updateStartButtonStyle();
+        $(window).resize(function(){
+            updateStartButtonStyle();
+        });
+
+        $('.start-btn').bind('click', function() {
+            machine.rand();
+        });
+
+        var textGo = $('');
+        $('.start-btn').mouseenter(function(){
+            $(this).children('i.fa-compass').hide();
+
+        });
+
     });
-
-    var textGo = $('');
-    $('.start-btn').mouseenter(function(){
-        $(this).children('i.fa-compass').hide();
-
-    });
-
-});
+})(jQuery, window, document);
