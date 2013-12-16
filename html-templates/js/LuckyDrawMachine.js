eeff8665f771bf1d7a3f765bf9114830dd3de5fe
mike.chung <mikechung4@gmail.com>
2013-12-16 13:59:28 +0800

    minor refacotring

---
+++ html-templates/js/LuckyDrawMachine.js
@@ -0,0 +1,50 @@
+var Machine = function(candidatesUpdateHandler, resultUpdateHandler) {
+    var updateCandidates = candidatesUpdateHandler;
+    var updateResult = resultUpdateHandler;
+    var sockjs_url = '/sock';
+    var sockjs = new SockJS(sockjs_url);
+
+    sockjs.onopen    = function() {};
+
+    sockjs.onmessage = function(e) {
+        var obj = JSON.parse(e.data);
+        // Super lazy implementation
+        if (obj.candidates) {
+            updateCandidates(obj.candidates);
+        } else if (obj.poorMan) {
+            updateResult(obj.poorMan);
+        }
+    };
+
+    sockjs.onclose = function() {};
+
+    function validateHandler(handler) {
+        if (!handler || typeof handler != "function") {
+            throw "Handler should be a function";
+        }
+    }
+
+    // The public API encapsulated the data accessing logic
+    return {
+        registerCandidatesUpdateHandler: function(handler) {
+            validateHandler(handler);
+            updateCandidates = handler;
+        },
+        registerResultUpdateHandler: function(handler) {
+            validateHandler(handler);
+            updateResult = handler;
+        },
+        addCandidate: function(v) {
+            $.get('/addCandidate', {'candidate': v});
+        },
+        removeCandidate: function (v) {
+            $.get('/removeCandidate', { 'candidate': v });
+        },
+        clearCandidates: function() {
+            $.get('/clearCandidates');
+        },
+        rand: function() {
+            $.get('/rand');
+        }
+    }
+};
---
+++ html-templates/js/candidateListComponent.js
@@ -0,0 +1,68 @@
+/** @jsx React.DOM */
+(function($, document, machine) {
+
+    var CandidateList = React.createClass({
+                                              render: function() {
+                                                  var onDelete = this.props.onDelete;
+                                                  var createItem = function(itemText) {
+                                                      return <li id={itemText}>{itemText}<span className="delete" title="Delete" onClick={onDelete.bind(this,itemText)}><i className="fa fa-minus-circle"></i></span></li>
+                                                  };
+                                                  return <ul className="item-list">{this.props.items.map(createItem)}</ul>;
+                                              }
+                                          });
+
+    var InputForm = React.createClass({
+                                          getInitialState: function() {
+                                              return {items: []};
+                                          },
+                                          componentDidMount: function() {
+                                              var dom = this.refs.candidateInput.getDOMNode();
+                                              dom.setAttribute("x-webkit-speech");
+
+                                              var reactCpn = this;
+                                              machine.registerCandidatesUpdateHandler(function(candidates) {
+                                                  reactCpn.setState({items: candidates});
+                                              });
+                                          },
+                                          handleAdd: function(e) {
+                                              e.preventDefault();
+                                              var val = this.refs.candidateInput.getDOMNode().value.trim();
+                                              this.refs.candidateInput.getDOMNode().value = "";
+                                              machine.addCandidate(val);
+                                          },
+                                          handleDelete: function(val){
+                                              machine.removeCandidate(val);
+                                          },
+                                          handleDeleteAll: function(e){
+                                              machine.clearCandidates();
+                                          },
+                                          handleInputDone: function(e){
+                                              $('.main-container').removeClass('show animated fadeOutUp');
+                                              $('.main-container').addClass('hide');
+                                              $('#start-view-container').addClass('show animated fadeInDown');
+                                          },
+                                          render: function() {
+                                              return (
+                                                  <div>
+                                                      <h1>Edit Items</h1>
+                                                      <form id="edit-item-form" onSubmit={this.handleAdd}>
+                                                          <input type="text" placeholder="Enter item name" id="new-candidate" ref="candidateInput"/>
+                                                          <button className="btn positive-btn" title="Add" onClick={this.handleAdd}><i className="fa fa-plus"></i></button>
+                                                          <div className="item-list-container">
+                                                              <h2>Items List</h2>
+                                                              <CandidateList items={this.state.items} onDelete={this.handleDelete}/>
+                                                              <div className="text-right"><a className="delete-all" onClick={this.handleDeleteAll}><i className="fa fa-times"></i>Delete All</a></div>
+                                                          </div>
+                                                          <div className="btn-set">
+                                                              <button className="btn primary-btn btn-done" onClick={this.handleInputDone}>Done</button>
+                                                          </div>
+                                                      </form>
+                                                  </div>
+                                                  );
+                                          }
+                                      });
+
+    React.renderComponent(
+        <InputForm />, document.getElementById('edit-item-container')
+    );
+})(jQuery, document, window.machine);
