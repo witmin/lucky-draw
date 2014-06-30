1991e2c6f256c8cf9e91b488985a0ea9dfa04d3a
millie.lin <millie.lin@netdimensions.com>
2014-06-30 11:40:19 +0800

    Clean up the css and scss files, updated config.rb settings. Deleted folder "www". Updated font-awesome lib to 4.1.0. Updated animation.min.css to 3.1.1

---
+++ App/config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'public/css'
+sass_dir = 'public/scss'
+images_dir = 'public/images'
+javascripts_dir = 'public/js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
