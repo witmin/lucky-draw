442d72d55dd843a2317af99a230b102260e98587
Keni Ng <kening@hk1.ibm.com>
2019-05-09 12:51:45 +0800

    Fix express.js error handler (need 4 parameters)

--- App/app.js
+++ App/app.js
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -54,7 +54,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
